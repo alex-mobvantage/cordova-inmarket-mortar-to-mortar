71dd7ba6c42b3b9132d1a0c59c72d8f4fa0fc29d
Alexander Kawrykow <azzro.boy@gmail.com>
2017-09-20 12:34:20 -0700

    adding logging for better debugability

--- src/android/M2MPlugin.java
+++ src/android/M2MPlugin.java
@@ -1,5 +1,7 @@
 package com.inmarket.cordova;
 
+import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
@@ -12,6 +14,8 @@ import android.content.Context;
 import com.inmarket.m2m.M2MBeaconMonitor;
 
 public class M2MPlugin extends CordovaPlugin {
+    private static final String TAG = "M2M";
+
     private static final String SET_USER_ID = "setUserId";
     private static final String SET_OPT_IN_FOR_PUSH = "setOptInForPush";
     private static final String SET_PUSH_TOKEN = "setPushToken";
@@ -27,33 +31,39 @@ public class M2MPlugin extends CordovaPlugin {
         final String userId = args.getString(0);
         M2MBeaconMonitor.setPublisherUserId(userId);
 
+        Log.d(TAG, "Set user id: " + userId);
         callbackContext.success();
         return true;
       } else if (action.equals(SET_OPT_IN_FOR_PUSH)){
         final Boolean optIn = args.getBoolean(0);
         M2MBeaconMonitor.setOptInForPush(optIn);
-        
+
+        Log.d(TAG, "Set opt in for push: " + optIn);
         callbackContext.success();
         return true;
       } else if (action.equals(SET_PUSH_TOKEN)){
         final String token = args.getString(0);
         M2MBeaconMonitor.setPushToken(this.cordova.getActivity().getApplicationContext(), token);
 
+        Log.d(TAG, "Set push token: " + token);
         callbackContext.success();
         return true;
       } else if (action.equals(START_SERVICE)){
         M2MBeaconMonitor.startService();
 
+        Log.d(TAG, "Started service");
         callbackContext.success();
         return true;
       } else if (action.equals(STOP_SERVICE)){
         M2MBeaconMonitor.stopService(this.cordova.getActivity().getApplicationContext());
 
+        Log.d(TAG, "Stopped service");
         callbackContext.success();
         return true;
       } else if (action.equals(REQUEST_LOCATION_PERMISSION)){
         M2MBeaconMonitor.requestLocationPermission(this.cordova.getActivity());
 
+        Log.d(TAG, "Requested location permission");
         callbackContext.success();
         return true;
       } else if (action.equals(CHECK_LOCATION_PERMISSION)){
@@ -61,6 +71,8 @@ public class M2MPlugin extends CordovaPlugin {
         
         final JSONObject result = new JSONObject();
         result.put("has_permission", hasPermission);
+
+        Log.d(TAG, "Checked location permission: " + hasPermission);
         callbackContext.success(result);
 
         return true;
@@ -68,9 +80,11 @@ public class M2MPlugin extends CordovaPlugin {
         final Boolean optIn = args.getBoolean(0);
         M2MBeaconMonitor.setOptInForGeofencing(optIn);
 
+        Log.d(TAG, "Set opt in for geofencing: " + optIn);
         callbackContext.success();
         return true;
       } else {
+        Log.e(TAG, "Unknown/unsupported action: " + action);
         callbackContext.error("Unknown/unsupported action");
       }
 
